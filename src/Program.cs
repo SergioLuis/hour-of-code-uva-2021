d7bd61b1a05216348f019a66c9948bc607d43478
Sergio Luis Para <sluis01@ucm.es>
2021-11-27 17:21:34 +0100

    First commit!

---
+++ src/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HourOfCodelse
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // This is your program's entry point
+        }
+    }
+}
